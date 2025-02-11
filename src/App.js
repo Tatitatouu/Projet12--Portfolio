b27b3eecb2e0ec40c45106bd5ea29152631f4d81
Tatitatouu <tatiana68270wi@gmail.com>
2025-02-11 11:32:58 +0100

    ajout basename

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/header/header"
 import HeroSection from "./components/hero/hero"
 import About from "./components/about/about";
@@ -9,7 +10,7 @@ import Footer from "./components/footer/footer";
 
 function App() {
   return (
-    <>
+    <Router basename="/Projet12--Portfolio">
       <Header />    
       <HeroSection />
       <About />
@@ -17,7 +18,7 @@ function App() {
       <Portfolio />
       <ContactForm />
       <Footer />
-    </>
+    </Router>
   );
 }
 
